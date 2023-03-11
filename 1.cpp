343a7363c83e80d5ae10c59ffaa4071d991243f8
ZlataProkurova <91047338+ZlataProkurova@users.noreply.github.com>
2023-03-12 01:20:04 +0300

    Add files via upload

---
+++ 1.cpp
@@ -0,0 +1,18 @@
+﻿// ConsoleApplication3.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <set>
+using namespace std;
+int main()
+{
+    int n, t;
+    cin >> n;
+    set<int> a;
+    for (int i = 0; i < n; ++i) {
+        cin >> t;
+        a.insert(t);
+    }
+    cout << a.size();
+    return 0;
+}
